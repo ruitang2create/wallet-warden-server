fa74071be2cf46f9f18136c521aaf86bb04422e7
ruitang2create <trtdytr93629@gmail.com>
2024-02-02 14:40:38 -0800

    add password encrypt/decrypt functions; implement basic CRUDUser class

--- auth/auth_security.py
+++ auth/auth_security.py
@@ -3,4 +3,9 @@ from passlib.context import CryptContext
 PWD_CONTEXT = CryptContext(schemes=["bcrypt"], deprecate="auto")
 
 
-def verify_password()
+def verify_password(unverified_password: str, hashed_password: str) -> bool:
+    return PWD_CONTEXT.verify(unverified_password, hashed_password)
+
+
+def hash_password(raw_password: str) -> str:
+    return PWD_CONTEXT.hash(raw_password)
--- db/crud/crud_user.py
+++ db/crud/crud_user.py
@@ -1,3 +1,21 @@
+from sqlalchemy.orm import Session
+
+from auth.auth_security import hash_password
 from db.crud.base import CRUDBase
 from db.models.user import User
 from db.schemas.user import CreateUserSchema
+
+
+class CRUDUser(CRUDBase[User, CreateUserSchema]):
+    def create(self, db: Session, *, obj_in: CreateUserSchema) -> User:
+        create_data = obj_in.dict()
+        create_data.pop("password")
+        db_obj = User(**create_data)
+        db_obj.hashed_password = hash_password(obj_in.password)
+        db.add(db_obj)
+        db.commit()
+
+        return db_obj
+
+
+User_Manager = CRUDUser(User)
