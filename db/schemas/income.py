aebcc587b3d70807ab84425a4a6998440836dc72
ruitang2create <trtdytr93629@gmail.com>
2024-02-12 00:18:47 -0800

    Refactor income types

--- db/schemas/income.py
+++ db/schemas/income.py
@@ -3,31 +3,31 @@ from typing import List
 from datetime import datetime
 
 
-class CreateIncomeSchema(BaseModel):
+class IncomeBase(BaseModel):
     source: str
     amount: float
     currency: str
     income_type: str
     transaction_time: datetime
     is_regular: bool
-    user_id: UUID4
     notes: str
 
 
-class GetIncomeSchema(BaseModel):
+class CreateIncomeRequestSchema(IncomeBase):
+    ...
+
+
+class CreateIncomeSchema(IncomeBase):
+    user_id: UUID4
+
+
+class IncomeResponseSchema(IncomeBase):
     id: UUID4
-    source: str
-    amount: float
-    currency: str
-    income_type: str
-    transaction_time: datetime
-    is_regular: bool
     user_id: UUID4
-    notes: str
 
     class Config:
         orm_mode: True
 
 
-class GetIncomeListSchema(BaseModel):
-    incomes: List[GetIncomeSchema]
+class IncomeListResponseSchema(BaseModel):
+    incomes: List[IncomeResponseSchema]
