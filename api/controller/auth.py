18990ec5ae9ece1d68c4b8bb1cc1db21aab3d035
ruitang2create <trtdytr93629@gmail.com>
2024-02-12 00:20:02 -0800

    refine auth endpoints

--- api/controller/auth.py
+++ api/controller/auth.py
@@ -17,7 +17,6 @@ def create_user_signup(
         db: Session = Depends(get_db),
         payload: CreateUserSchema,
 ) -> Any:
-    # user = db.query(User).filter(User.username = payload.username).first()
     user = User_Manager.get_by_username(db=db, username=payload.username)
     if user:
         raise HTTPException(
